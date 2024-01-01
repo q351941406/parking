ad90d742c669bb68e6b787a8cd3b906c15f79ca4
yjb <1037779408@qq.com>
2024-01-01 18:18:31 +0800

    2

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,25 +1,41 @@
 <template>
-  <div
-    style="background: #000; width: 100vw; height: 100vh; padding-top: 30px"
-  >
- 
+  <div style="background: #000; width: 100vw; height: 100vh; padding-top: 30px">
     <div class="top">
       <div>
         <div style="display: flex">
           <img src="../assets/avator.jpg" alt="" class="avator" />
-          <div class="tip">Hello,David</div>
+          <div class="tip" @click="ifLogin=true">{{userPhone}}</div>
         </div>
       </div>
       <div>
         <img src="../assets/设置.png" alt="" width="28px" />
       </div>
     </div>
-    <div class="content">
-      <div class="img1Container">
-        <img src="../assets/1.jpg" alt="" class="img1" @click="handleOpen" />
+    <div class="content"  v-for="(item,index) in channel" :key="index" >
+      <div class="img1Container" v-if="index==0">
+        <div>
+          <img :src="imageUlr" alt="" class="img1" @click="handleOpen(item)" />
         <div class="title">
           <div>
-            <div class="title_title">Guest Room</div>
+            <div class="title_title">{{item.channelName}}</div>
+            <div class="title_detail">1 Device - Active</div>
+          </div>
+          <div>
+            <img
+              src="../assets/箭头.png"
+              alt=""
+              class="jiantou"
+              @click="handleOpen"
+            />
+          </div>
+        </div> 
+        </div>
+      </div>
+      <div class="img2Container" v-else>
+        <img :src="imageUlr" alt="" class="img1" @click="handleOpen(item)" />
+        <div class="title">
+          <div>
+            <div class="title_title">{{item.channelName}}</div>
             <div class="title_detail">1 Device - Active</div>
           </div>
           <div>
@@ -32,100 +48,112 @@
           </div>
         </div>
       </div>
-      <div class="img2Container">
-        <img src="../assets/1.jpg" alt="" class="img1" @click="handleOpen" />
-        <div class="title">
-          <div>
-            <div class="title_title">Guest Room</div>
-            <div class="title_detail">1 Device - Active</div>
-          </div>
-          <div>
-            <img
-              src="../assets/箭头.png"
-              alt=""
-              class="jiantou"
-              @click="handleOpen"
-            />
-          </div>
+    
         </div>
       </div>
-      <div class="img2Container">
-        <img src="../assets/1.jpg" alt="" class="img1" @click="handleOpen" />
-        <div class="title">
-          <div>
-            <div class="title_title">Guest Room</div>
-            <div class="title_detail">1 Device - Active</div>
-          </div>
-          <div>
-            <img
-              src="../assets/箭头.png"
-              alt=""
-              class="jiantou"
-              @click="handleOpen"
-            />
-          </div>
-        </div>
-      </div>
-
-      <div class="img2Container">
-        <img src="../assets/1.jpg" alt="" class="img1" @click="handleOpen" />
-        <div class="title">
-          <div>
-            <div class="title_title">Guest Room</div>
-            <div class="title_detail">1 Device - Active</div>
-          </div>
-          <div>
-            <img
-              src="../assets/箭头.png"
-              alt=""
-              class="jiantou"
-              @click="handleOpen"
-            />
-          </div>
-        </div>
-      </div>
-
-      <div class="img2Container">
-        <img src="../assets/1.jpg" alt="" class="img1" @click="handleOpen" />
-        <div class="title">
-          <div>
-            <div class="title_title">Guest Room</div>
-            <div class="title_detail">1 Device - Active</div>
-          </div>
-          <div>
-            <img
-              src="../assets/箭头.png"
-              alt=""
-              class="jiantou"
-              @click="handleOpen"
-            />
-          </div>
-        </div>
-      </div>
+   
+  <div class="phoneContainer" v-show="ifLogin">
+    <div class="phone">
+      <input type="text" placeholder="请输入手机号" v-model="phoneNum" />
+      <input type="submit" value="提交" @click="login" />
     </div>
   </div>
-  <div v-show="showVideoModal" class="videoModal" >
+  <div v-show="showVideoModal" class="videoModal">
     <!-- 在这里放置全屏播放的视频组件 -->
     <!-- 例如：<VideoPlayer :src="videoSrc" /> -->
- <div >
-  <div><img src="../assets/关闭.png" class="close" alt="" @click="closeVideoModal"></div>
-  <div id='playWind1' class="player" ></div>
- </div>
-    
-  
-
+    <div>
+      <div>
+        <img
+          src="../assets/关闭.png"
+          class="close"
+          alt=""
+          @click="closeVideoModal"
+        />
+      </div>
+      <div id="playWind1" class="player"></div>
+    </div>
   </div>
 </template>
 <script setup lang="ts">
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
 import EZUIKit from "ezuikit-js";
+import axios from "axios";
+const ifLogin = ref(true);
+const phoneNum = ref();
+const userPhone=ref('未登录')
+const channel=ref();
+ const phoneNumber = localStorage.getItem('phoneNumber');
+userPhone.value=phoneNumber==null?'未登录':phoneNumber
+ if(phoneNumber!=null){
+  ifLogin.value=false;
+ }
+ userPhone.value = phoneNumber == undefined ? '未登录' : phoneNumber;
+
+if (phoneNumber !== undefined) {
+  ifLogin.value = false;
+}
+const  getVideos=()=> {
+   
+
+      const apiUrl = `https://fc.mayizuanqian.com/getVideos?phone=${userPhone.value}`; // 将<phone>替换为实际的电话号码
+
+      axios.get(apiUrl)
+        .then(response => {
+              console.log(response)
+              channel.value=response.data.data;
+   
+        })
+        .catch(error => {
+        
+        });
+   
+  }
+getVideos()
+const login = () => {
+
+  var data = JSON.stringify({
+    phone:phoneNum.value,
+  });
+  onMounted(()=>{
+   
+  })
+
+  var config = {
+    method: "post",
+    url: "https://fc.mayizuanqian.com/submitPhone",
+    headers: {
+
+      "Content-Type": "application/json",
+      Accept: "*/*",
+      Host: "fc.mayizuanqian.com",
+      Connection: "keep-alive",
+    },
+    data: data,
+  };
+
+  axios(config)
+    .then(function (response) {
+     
+      console.log(JSON.stringify(response.data));
+      userPhone.value=phoneNum.value;
+      localStorage.setItem('phoneNumber', phoneNum.value);
+   
+      ifLogin.value=false;
+      getVideos();
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
+
+
 const showVideoModal = ref(false);
-const handleOpen = () => {
+const handleOpen = (item) => {
+    // console.log(item.url)
+    // debugger
   showVideoModal.value = true;
   var width = document.documentElement.clientWidth;
   var height = (document.documentElement.clientWidth * 9) / 16;
- 
-  
 
   const ezopenInit = (elementId, url, accessToken) => {
     const player = new EZUIKit.EZUIKitPlayer({
@@ -141,40 +169,86 @@ const handleOpen = () => {
   };
   const videoElement = document.getElementById("playWind1");
 
-
   // Initialize the first player
   window.EZOPENDemo1 = ezopenInit(
     "playWind1",
-    "ezopen://open.ys7.com/BB8967325/1.hd.live",
-    "at.ac3w8280awshig7d6u119gd6ajkgd8pb-1vybn8xky0-1iz72vc-jccsbwdjp"
-  );
-
-  // Initialize the second player
-  window.EZOPENDemo2 = ezopenInit(
-    "playWind2",
-    "ezopen://open.ys7.com/BB8967325/2.hd.live",
+     item.url,
     "at.ac3w8280awshig7d6u119gd6ajkgd8pb-1vybn8xky0-1iz72vc-jccsbwdjp"
   );
 };
 
 const closeVideoModal = (event) => {
- 
-    showVideoModal.value = false;
- 
+  showVideoModal.value = false;
 };
 </script>
 <style scoped>
-.close{
+.phoneContainer {
+  position: relative;
+}
+
+.phoneContainer::before {
+  content: "";
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.7); /* 蒙版的颜色和透明度 */
+  z-index: 110;
+}
+.phone {
+  position: fixed;
+  left: 50%;
+  top: 30%;
+  transform: translate(-50%, -50%);
+  z-index: 111;
+  background: #fff;
+  width: 300px;
+  height: 150px;
+  border-radius: 10px;
+}
+.phone input[type="text"] {
+  width: 80%;
+  height: 40px;
+  margin-top: 40px;
+  position: relative;
+  left: 50%;
+  transform: translateX(-50%);
+
+  font-size: 16px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  outline: none;
+}
+
+.phone input[type="submit"] {
+  display: block;
+  width: 100px;
+  height: 30px;
+  margin: 0 auto;
+  margin-top: 10px;
+  background: #007bff;
+  color: #fff;
+  font-size: 14px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.phone input[type="submit"]:hover {
+  background: #0056b3;
+}
+.close {
   position: fixed;
   z-index: 1111;
   top: 10px;
   right: 10px;
   width: 20px;
 }
- .player {
-      margin-bottom: 15px;
-      margin-top:30px;
-    }
+.player {
+  margin-bottom: 15px;
+  margin-top: 30px;
+}
 .top {
   padding: 0 20px 20px;
   width: 100vw;
@@ -226,7 +300,7 @@ const closeVideoModal = (event) => {
   position: fixed;
   top: 0;
   left: 0;
-  width:100vw;
+  width: 100vw;
   height: 100vh;
   z-index: 2222;
   background-color: rgba(0, 0, 0, 0.8);
@@ -268,13 +342,13 @@ const closeVideoModal = (event) => {
   margin-top: 8px;
   transform: rotate(135deg);
 }
-::v-deep #playWind1-wrap{
+::v-deep #playWind1-wrap {
   z-index: 11;
 }
-::v-deep #playWind2-wrap{
-    margin-top:20px;
+::v-deep #playWind2-wrap {
+  margin-top: 20px;
 }
-::v-deep #playWind1-loading-item-txt{
-  margin-top:40px ;
+::v-deep #playWind1-loading-item-txt {
+  margin-top: 40px;
 }
 </style>
