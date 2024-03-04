2e6aae7236672da5ce18db5bc5a13ec0d5baae6f
甘正威 <351941406@qq.co>
2024-03-05 01:29:45 +0800

    no message

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,16 @@ import '@authing/guard-vue3/dist/esm/guard.min.css'
 import store from '../src/components/Store.js'
 
 
+(async function init() {
+    // 执行异步操作来加载用户信息
+    await store.dispatch('fetchUserInfo');
 
+
+})() // 这个额外的 `()` 使函数立即执行
+    .catch(() => {
+        // 这里处理任何在初始化过程中抛出的错误
+        console.error('Failed to initialize the app:');
+    });
 
 const domain = `${window.location.origin}/callback`
 // console.log('dumain',dumain);
@@ -20,7 +29,7 @@ const domain = `${window.location.origin}/callback`
 const guardOptions: GuardOptions = {
     appId: '65d8b5d805fe059574b1ff72',
     // redirectUri:`${import.meta.env.VITE_APP_API_BASE_URL}/auth/loginCallback`,
-    redirectUri:domain,// 官网后台可以配置多个,但这里指定其中一个
+    redirectUri: domain,// 官网后台可以配置多个,但这里指定其中一个
 }
 
 const app = createApp(App)
@@ -35,12 +44,5 @@ app.use(store)
 //     await store.dispatch('fetchUserInfo') // 在这里调用action获取用户信息
 // })
 // await store.dispatch('fetchUserInfo') // 在这里调用action获取用户信息
-async function init() {
-    // 如果有必要的话，执行异步操作来加载用户信息
-    await store.dispatch('fetchUserInfo');
-  
-   
-  }
-// 初始化应用
-await init();
+
 app.mount('#app')
