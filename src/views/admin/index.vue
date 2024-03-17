08aa014f4a9b9a001fac43686f6b49e045a57917
Sept-luo <114335262+Sept-luo@users.noreply.github.com>
2024-03-17 23:49:08 +0800

    订单列表添加字段&新增机场列表

--- src/views/admin/index.vue
+++ src/views/admin/index.vue
@@ -5,6 +5,9 @@
       <el-tab-pane label="用户列表">
         <User />
       </el-tab-pane>
+      <el-tab-pane label="机场列表">
+        <AirportList />
+      </el-tab-pane>
     </el-tabs>
   </div>
 </template>
@@ -14,6 +17,7 @@ import { ElTabs, ElTabPane } from "element-plus";
 import "element-plus/dist/index.css";
 import User from "./UserList/index.vue";
 import Order from "./OrderList/index.vue";
+import AirportList from "./AirportList/index.vue";
 </script>
 
 <style scoped></style>
